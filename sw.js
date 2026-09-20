3b291c1b4a85a51c5c29c4702dfc30b6c47e61f9
moulinanthony60-creator <moulinanthony60@gmail.com>
2026-09-20 18:35:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='one-mobile-alpha-1484-collection100-party-v32';
-const CORE=['./ui/icons.js?v=01484','./ui/foundation.css?v=01484','./ui/foundation.js?v=01484','./','./index.html','./manifest.json','./manifest.webmanifest','./privacy.html','./terms.html','./icon-192.png','./icon-512.png'];
+const CACHE='one-mobile-alpha-1493-scratch-cards-v32';
+const CORE=['./ui/icons.js?v=01493','./ui/foundation.css?v=01493','./ui/foundation.js?v=01493','./','./index.html','./manifest.json','./manifest.webmanifest','./privacy.html','./terms.html','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("one-mobile-alpha-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
